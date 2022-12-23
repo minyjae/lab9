d8c99bf157c85b1178a699953e3978855040b174
fdfsdf <plpkokoko@gmail.com>
2022-12-23 15:40:34 +0700

    complete lab9

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,50 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    int age,h;
+    int b;
+    string c;
+    cout << "Enter your age: ";
+    cin >> age;
+    cin.ignore();
+        if(age <= 25) {
+            cout << "Enter your height: ";
+            cin >> h;
+                if(h < 100){
+                    c = "Chopper";
+                }
+                else if(h < 180){
+                    c = "Usopp";
+                }
+                else {
+                    cout << "Enter your bounty: ";
+                    cin >> b;
+                        if(b > 1.1e9) {
+                            c = "Zoro";
+                        }
+                        else {
+                            c = "Sanji";
+                        }
+                }
+        }
+        else if(age <= 60){
+            cout << "Enter your bounty: ";
+            cin >> b;
+                if(b > 5e8) {
+                    c = "Jinbe";
+                }
+                else {
+                    c = "Franky";
+                }
+        }
+        else {
+            c = "Brook";
+        }
+        cout << "Your character = ";
+        cout << c;
+        return 0;
+}
+
+
